c9af877686fc051929a89cb2fd9dde14bfb07d5d
Gabriela Porzuczek <gporzuczek21@gmail.com>
2025-09-14 20:14:11 +0200

    second send

--- read.py
+++ read.py
@@ -1,19 +1,5 @@
-import sqlite3
-from sqlite3 import Error
+from create import create_connection
 
-def create_connection(db_file):
-   """ create a database connection to the SQLite database
-       specified by the db_file
-   :param db_file: database file
-   :return: Connection object or None
-   """
-   conn = None
-   try:
-       conn = sqlite3.connect(db_file)
-   except Error as e:
-       print(e)
-
-   return conn
 def select_all(conn, table):
    """
    Query all rows in the table
